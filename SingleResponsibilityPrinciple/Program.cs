48803586cf3f379e1f25bd0c9bb2f474751d8b7f
Owner <Owner@DESKTOP-DE9633A>
2017-11-07 21:02:00 -0600

    Request 407: Read trades from a remote call to web services

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -10,6 +10,7 @@ namespace SingleResponsibilityPrinciple
         static void Main(string[] args)
         {
             var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
+            string url = "http://faculty.css.edu/tgibbons/trades4.txt";
 
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
@@ -17,8 +18,10 @@ namespace SingleResponsibilityPrinciple
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
+            var URLTradeDataProvider = new URLTradeDataProvider(url);
 
             var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+            
             tradeProcessor.ProcessTrades();
 
             Console.ReadKey();
---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using SingleResponsibilityPrinciple.Contracts;
+using System.Net;
+
+//Added a new class to read the trades from a remote call to the web service
+namespace SingleResponsibilityPrinciple
+{
+    public class URLTradeDataProvider:ITradeDataProvider
+    {
+        public URLTradeDataProvider(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public URLTradeDataProvider(string url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            string url = null;
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+
+        private readonly Stream stream;
+        private string url;
+    }
+}
